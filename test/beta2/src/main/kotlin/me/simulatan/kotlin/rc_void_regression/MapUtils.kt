86b17193fc96ab0efe633ad83e02f28af3f93b8c
SIMULATAN <simulatan2319@gmail.com>
2024-11-17 21:20:31 +0100

    Rewrite `getOrDefault` fun to be more idiomatic

--- test/beta2/src/main/kotlin/me/simulatan/kotlin/rc_void_regression/MapUtils.kt
+++ test/beta2/src/main/kotlin/me/simulatan/kotlin/rc_void_regression/MapUtils.kt
@@ -1,10 +1,8 @@
 package me.simulatan.kotlin.rc_void_regression
 
-fun <T> Map<Any, Any>.getOrDefault(key: Any, defaultValue: T): T {
-	if (!containsKey(key)) {
-		return defaultValue
-	}
-	return get(key) as T
+fun <T> Map<Any, Any>.getOrDefault(key: Any, defaultValue: T): T = when {
+	containsKey(key) -> get(key) as T
+	else -> defaultValue
 }
 
 inline fun <reified T : Any> Map<Any, Any>.getKotlinDefaultJava(): T? = getOrDefault(T::class, null) ?: getOrDefault(T::class.java, null)
